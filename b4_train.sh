f93d6993516b1561e22818ded7bf26b92d5478b1
xiaoshideta <86964787+xiaoshideta@users.noreply.github.com>
2024-04-03 22:37:21 +0800

    Add files via upload

--- b4_train.sh
+++ b4_train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 nyu_b4_train.py --port=29516 --distillation_alpha=1.0 --distillation_flag=1 --lambda_mask=0.75
+CUDA_VISIBLE_DEVICES=1,3 python -m torch.distributed.launch --nproc_per_node=2 nyu_b4_train.py --port=29516 --distillation_alpha=1.0 --distillation_beta=0.1 --distillation_flag=1 --lambda_mask=0.75 --select="max" --mask_single="hint"
